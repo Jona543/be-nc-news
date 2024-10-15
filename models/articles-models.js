31903539f466ccb739a8ab04e40001c8ec2f9fe1
Jonathan Tromp <jonatromp45@gmail.com>
2024-10-15 14:04:20 +0100

    add changes mentioned on feedback to article_id endpoint

--- models/articles-models.js
+++ models/articles-models.js
@@ -2,8 +2,11 @@ const db = require("../db/connection")
 
 const fetchArticleById = (article_id) => {
     return db.query('SELECT * FROM articles WHERE article_id = $1;', [article_id])
-    .then((result) => {
-        return result.rows
+    .then(({ rows }) => {
+        if (rows.length === 0){
+            return Promise.reject({status: 404, message: "Article Not Found"})
+        }
+        return rows[0]
     })
 }
 
